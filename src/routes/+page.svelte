61a32b5772e8d9fd4fc15be3d42bda7e73956d66
Carson Case <carsoncase@gmail.com>
2024-03-18 19:30:29 -0700

    website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<body class="background text-white md:text-black">
+    <div class="p-10">
+        <p class="text-7xl md:text-9xl gtaFont">$MIA Coin</p>
+        <p class="text-3xl md:text-5xl py-6">Official Meme Coin of the City of <br> Miami, Florida</p>    
+    </div>
+    <div class="h-lvh flex items-center gap-2 md:gap-9 justify-center">
+        <button class="outrun-button">Telegram</button>
+        <button class="outrun-button">Twitter</button>
+        <button class="outrun-button">Chart</button>
+    </div>
+
+</body>
