c88d9d5fe69e2abbdb31f9de1a27a01e394ddc05
Carson Case <carsoncase@gmail.com>
2024-03-19 00:17:42 -0700

    vibe button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
+<script>
+    let audio;
+    
+    const playAudio = () => {
+      audio.play();
+    };
+</script>
+
 <body class="background text-white md:text-black">
     <div class="p-10">
         <p class="text-7xl md:text-9xl gtaFont">$MIA Coin</p>
-        <p class="text-3xl md:text-5xl py-6">Official Meme Coin of the City of <br> Miami, Florida</p>    
+        <p class="text-3xl md:text-5xl py-6">Official Meme Coin of the City of <br> Miami, Florida</p> 
+        <button on:click={playAudio} class="outrun-button">Vibe</button> 
+        <audio src="/She.mp3" bind:this={audio}></audio>
     </div>
     <div class="h-lvh flex items-center gap-2 md:gap-9 justify-center">
         <button class="outrun-button">Telegram</button>
@@ -10,3 +20,14 @@
     </div>
 
 </body>
+
+<style>
+    @font-face {
+    font-family: gta;
+    src: url("/fonts/pricedown/pricedown bl.otf");
+    }
+
+    .gtaFont{
+        font-family: gta;
+    }
+</style>
