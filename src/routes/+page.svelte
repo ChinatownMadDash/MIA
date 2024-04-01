393820544f0763428416c0a26635beb3ef5f55d0
Carson Case <carsoncase@gmail.com>
2024-03-31 17:57:15 -0700

    replaced MIA with MIAMI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <body class="background text-white md:text-black">
     <div class="p-10">
-        <p class="text-7xl md:text-9xl gtaFont">$MIA Coin</p>
+        <p class="text-7xl md:text-9xl gtaFont">$MIAMI Coin</p>
         <p class="text-3xl md:text-5xl py-6">Official Meme Coin of the City of <br> Miami, Florida</p> 
         <button on:click={playAudio} class="outrun-button">Vibe</button> 
         <audio src="/She.mp3" bind:this={audio}></audio>
